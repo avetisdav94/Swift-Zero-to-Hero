138ad1b482dee7d7021eb1c4f5ccf422f8fabc29
avetisdav94 <avetisdav94@gmail.com>
2025-12-22 20:12:13 +0100

    Add Lesson 1: Variables and Constants basics

---
+++ Module_1_Fundamentals/Lesson_1_Variables.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let name = "Atlas"
+let characterClass = "Witcher"
+let race = "Human"
+
+var level = 2
+var xp = 44.5
+var hp = 100
+
+print("Hero \(name) \(race) \(characterClass)")
+print("Stats: Level \(level) | HP: \(hp) | XP: \(xp)")
