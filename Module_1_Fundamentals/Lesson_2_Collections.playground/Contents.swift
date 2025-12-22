ac74be991f816cc9cbaad34f29fd92bbe003c9e9
avetisdav94 <avetisdav94@gmail.com>
2025-12-22 21:23:28 +0100

    Implement inventory system and fix logic bug

--- Module_1_Fundamentals/Lesson_2_Collections.playground/Contents.swift
+++ Module_1_Fundamentals/Lesson_2_Collections.playground/Contents.swift
@@ -1,3 +1,60 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var hp = 100
+
+// Массивы
+var inventory =  ["Sword", "Shield", "Potion"]
+var healthText = ["Game over","Low Health", "Ready to fight"]
+// Добавление
+inventory.append("Map")
+
+// Удаление
+inventory.remove(at: 0)
+
+print(inventory)
+
+
+// Условия
+
+if hp <= 0 {
+    print(healthText[0])
+} else if hp < 20{
+    print("Warning:  \(healthText[1])")
+} else {
+    print(healthText[2])
+}
+
+// Проверка на то пустой ли массив или заполнен чем то
+if inventory.isEmpty {
+    print(" Our Arrays is Empty")
+} else {
+    print("We have some item in out array")
+}
+
+
+// MARK: Практическое задание: "Inventory System"
+
+var gold: Int
+
+// Герой нашел сундук с новым предметом
+inventory.append("Legendary Amulet")
+gold = 50
+print("Сейчас в инвентаре \(inventory.count) предмета")
+
+// Battle
+hp = 100 - 40
+
+if hp > 50 {
+    print("Герой в порядке")
+}else if hp <= 50 {
+    print("Нужно лечение!")
+}else if hp <= 0 {
+    print("Герой погиб...")
+}
+
+if inventory.count > 1 {
+    let soldItem = inventory.remove(at: 1)
+    print("Sold: \(soldItem)")
+}
+
+print("Inventory: \(inventory)")
